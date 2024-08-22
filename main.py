627082f96fa2ea2435da7a7114297eb771cca700
kels7 <hiketabo@gmail.com>
2024-08-22 08:21:58 +0000

    refactor: restructure to ensure contractName is returned

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 from ariadne.asgi import GraphQL
-from ariadne import (
-    load_schema_from_path,
-    make_executable_schema,
-    QueryType,
-    snake_case_fallback_resolvers
-)
-from resolvers import resolve_contract
+from ariadne import load_schema_from_path, make_executable_schema
+from resolvers import query
 
-query = QueryType()
-
-# Bind resolver
-query.set_field("contract", resolve_contract)
-
-# load schema
 type_defs = load_schema_from_path("schema.graphql")
-schema = make_executable_schema(type_defs, query, snake_case_fallback_resolvers)
-
+schema = make_executable_schema(type_defs, query)
 
 app = GraphQL(schema, debug=True)
--- resolvers.py
+++ resolvers.py
@@ -1,6 +1,13 @@
-def resolve_contract(_, info):
+from ariadne import QueryType
+
+query = QueryType()
+
+@query.field("contract")
+def resolve_contract(*_, name):
     return {
+        "contractName": name,
         "code": "<CODE>",
         "methods": ["transfer", "transfer_from", "balanceOf"],
         "vars": ["balance", "token_supply", "owner"]
     }
+
